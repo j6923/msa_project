7c39c8dcfe1c9d97f79b6fef5419a85c73bb08d7
230jeonminsoo <95909596+230jeonminsoo@users.noreply.github.com>
2021-12-12 19:12:27 +0900

    Update login.js

--- project_main/js/login.js
+++ project_main/js/login.js
@@ -1,25 +1,14 @@
-<script>window.addEventListener('load', function(){
-            let windowId; 
-            let btObjArr= document.querySelectorAll('button');
-            btObjArr[0].addEventListener('click',function(){
-                let url = 'popup.html';
-                let target = 'firstw';
-                //그 창을 또 띄우지 않기 위해서 이름을 준다. 
-                //기존창의 내용이 변경된다. 
-                let features = 'width=300px, height=300px';
-                windowId= window.open(url, target, features);  
-                //지금 열려진 창을 닫아야해서 창의 변수명이 필요하다. 
-                btObjArr[0].style.display = 'none';
-                btObjArr[1].style.display = 'inline-block';
-            });    
-            btObjArr[1].addEventListener('click',function(){
-                windowId.close(); //새창이 닫힌다 
-                btObjArr[1].style.display = 'none';
-                btObjArr[0].style.display = 'inline-block';
-            });
-                
-            
-            
+ <script>
+  function login_js(){
+	    let $loginFormObj = $('header>session>div.login_item>ul.login>li.log_li>a#login');
+	    $signupFormObj.click(function () {
+	        let url = './login.html';
+	        let target = 'login';
+	        let features = 'width=300px, height=300px';
+	        windowId= window.open(url, target, features);  
+	        
+	    });
 
-        });
-        </script>
+	}
+  
+  </script>
