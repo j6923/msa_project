a424f1a714b93192f12b4a86e18a867a372dcc1f
230jeonminsoo <95909596+230jeonminsoo@users.noreply.github.com>
2021-12-12 19:36:48 +0900

    Update login.js

--- project_main/js/login.js
+++ project_main/js/login.js
@@ -1,7 +1,7 @@
 
   function login_js(){
 	    let $loginFormObj = $('header>session>div.login_item>ul.login>li.log_li>a#login');
-	    $signupFormObj.click(function () {
+	    $loginFormObj.click(function () {
 	        let url = './login.html';
 	        let target = 'login';
 	        let features = 'width=300px, height=300px';
