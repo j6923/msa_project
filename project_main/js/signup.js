0b3eee82c60392e2a859604a505b6b242eb417dd
j6923 <69246174+j6923@users.noreply.github.com>
2021-12-13 15:56:52 +0900

    Add files via upload

--- project_main/js/signup.js
+++ project_main/js/signup.js
@@ -1,14 +1,12 @@
-
-
-function signup_js(){
-    let $signupFormObj = $('header>session>div.login_item>ul.login>li.log_li>a#signup');
+// signup.html에서 실행되는 js파일입니다. 
+function login_js(){
+    let $signupFormObj = $('p>a');
     $signupFormObj.click(function () {
-        let url = './signup.html';
-        let target = 'signup';
+        let url = './login.html';
+        let target = 'login';
         let features = 'width=300px, height=300px';
         windowId= window.open(url, target, features);  
         
     });
 
 }
-
