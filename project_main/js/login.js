ce6243b5398896181237479b8e6b31daba3b0c74
j6923 <69246174+j6923@users.noreply.github.com>
2021-12-15 08:22:40 +0900

    Add files via upload

--- project_main/js/login.js
+++ project_main/js/login.js
@@ -1,7 +1,7 @@
 
   function login_js(){
 	    let $loginFormObj = $('header>session>div.login_item>ul.login>li.log_li>a#login');
-	    $loginFormObj.click(function () {
+	    $signupFormObj.click(function () {
 	        let url = './login.html';
 	        let target = 'login';
 	        let features = 'width=300px, height=300px';
@@ -10,3 +10,4 @@
 	    });
 
 	}
+  
