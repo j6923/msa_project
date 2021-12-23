35170af612ab6644efd2ab14df37253b0e29dc47
CHIHYUNGLEE <eno125@naver.com>
2021-12-24 01:20:24 +0900

    rne

---
+++ src/main/java/com/my/notice/vo/Notice.java
@@ -0,0 +1,87 @@
+package com.my.notice.vo;
+
+import java.util.Date;
+
+public class Notice {
+	private int ntcIdx;
+	private String uNickName;
+	private String ntcTitle;
+	private String ntcContent;
+	private String ntcAttachment;
+	private int ntcViews;
+	private Date ntcCreateAt;
+	//private int brdThumbUp; 
+	
+	//멤버변수들의 접근제어자를 private으로 하면 setter, getter 메소드들이 필요하고 생성자를 통해서 초기화하는 작업도 필요하다.
+    //매개변수없는 생성자 만듬
+	public Notice() {	  
+	}
+	
+	public Notice(int ntcIdx, String uNickName, String ntcTitle, String ntcContent, String ntcAttachment, int ntcViews, Date ntcCreateAt) {
+		super();
+		this.ntcIdx = ntcIdx;
+		this.uNickName = uNickName;
+		this.ntcTitle = ntcTitle;
+		this.ntcContent = ntcContent;
+		this.ntcAttachment = ntcAttachment;
+		this.ntcViews = ntcViews;
+		this.ntcCreateAt = ntcCreateAt;
+	}
+	
+	public int getNtcIdx() {
+		return ntcIdx;
+	}
+
+	public void setNtcIdx(int ntcIdx) {
+		this.ntcIdx = ntcIdx;
+	}
+	
+	public String getUNickName() {
+		return uNickName;
+	}
+
+	public void setUNickName(String uNickName) {
+		this.uNickName = uNickName;
+	}
+	
+	public String getNtcTitle() {
+		return ntcTitle;
+	}
+
+	public void setNtcTitle(String ntcTitle) {
+		this.ntcTitle = ntcTitle;
+	}
+	
+	public String getNtcContent() {
+		return ntcContent;
+	}
+
+	public void setNtcContent(String ntcContent) {
+		this.ntcContent = ntcContent;
+	}
+	
+	public String getNtcAttachment() {
+		return ntcAttachment;
+	}
+
+	public void setNtcAttachment(String ntcAttachment) {
+		this.ntcAttachment = ntcAttachment;
+	}
+	
+	public int getNtcViews() {
+		return ntcViews;
+	}
+
+	public void setNtcViews(int ntcViews) {
+		this.ntcViews = ntcViews;
+	}
+	
+	public Date getNtcCreateAt() {
+		return ntcCreateAt;
+	}
+
+	public void setNtcCreateAt(Date ntcCreateAt) {
+		this.ntcCreateAt = ntcCreateAt;
+	}
+	
+}
