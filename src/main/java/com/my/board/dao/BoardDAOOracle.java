2884fcba5b735a666cc01125ccf6eae0fe1c8bdf
CHIHYUNGLEE <eno125@naver.com>
2021-12-24 16:33:06 +0900

    Update BoardDAOOracle.java

--- src/main/java/com/my/board/dao/BoardDAOOracle.java
+++ src/main/java/com/my/board/dao/BoardDAOOracle.java
@@ -14,7 +14,6 @@ import com.my.exception.AddException;
 import com.my.exception.FindException;
 import com.my.exception.ModifyException;
 import com.my.exception.RemoveException;
-import com.my.notice.vo.Notice;
 import com.my.sql.MyConnection;
 
 
