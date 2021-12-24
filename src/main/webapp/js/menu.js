bc4a6283a5fa57ffcbc1f1738e5790a3dedd18f2
CHIHYUNGLEE <eno125@naver.com>
2021-12-24 21:59:10 +0900

    Create menu.js

---
+++ src/main/webapp/js/menu.js
@@ -0,0 +1,51 @@
+function login_js(){
+    let $signupFormObj = $('div.menu>button.login');
+    $signupFormObj.click(function () {
+        let url = './login.html';
+        let target = 'login';
+        let features = 'width=300px, height=300px';
+        windowId= window.open(url, target, features);  
+        
+    });
+
+}
+/**
+ * 메뉴가 클릭되었을때
+ */
+ function menuClick(){
+    let $menuObj = $('header>nav>ul>li>a');
+    $menuObj.click(function(){
+        let menuHref = $(this).attr('href'); //href속성값 얻기(여기서 this는 클릭된 메뉴 객체를 의미)
+        console.log("메뉴 href=" + menuHref);
+
+
+        let ajaxUrl = ""; //요청할URL
+        let ajaxMethod = ""; //요청방식
+        switch(menuHref){
+			//로그인클릭시 팝업
+			case 'login.html':
+			let url='login.html';
+			let target='login';          
+			let features = 'width=300px, height=300px';
+			window.open(url,target,features);
+			
+			//menu에서 커뮤니티가 클릭되었을때
+            case 'ntclist':
+                ajaxUrl = menuHref;
+                $('section>div.articles').load(ajaxUrl, function(responseText, textStatus, jqXHR){
+                    if(jqXHR.status != 200){
+                        alert('응답실패:' + jqXHR.status);            
+                    }    
+                }); 
+                return false;
+
+            //menu에서 마이페이지가 클릭되었을때
+
+          }
+    });
+ }
+
+
+
+
+
