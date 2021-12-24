060fd5b7932792e28dbed09dd03f342b00cef41f
CHIHYUNGLEE <eno125@naver.com>
2021-12-24 11:12:29 +0900

    Create NoticeService.java

---
+++ src/main/java/com/my/notice/service/NoticeService.java
@@ -0,0 +1,52 @@
+package com.my.notice.service;
+
+import java.util.List;
+
+import com.my.exception.AddException;
+import com.my.exception.ModifyException;
+import com.my.exception.RemoveException;
+import com.my.notice.dao.NoticeDAOOracle;
+import com.my.notice.vo.Notice;
+import com.my.exception.FindException;
+
+public class NoticeService {
+	private NoticeDAOOracle dao = NoticeDAOOracle.getinstance();
+	private static NoticeService service = new NoticeService();
+	private NoticeService() {
+		
+	}
+	public static NoticeService getinstance() {
+		return service;
+	}
+	
+	
+	//관리자만 글 수정,삭제 ,추가 가능하게 바꿔야함.
+	public void addNtc(Notice n) throws AddException{
+		dao.addNtc(n);
+	}
+	
+	public List<Notice> findNtcAll() throws FindException {
+		return dao.findNtcAll();
+	}
+	
+	public Notice findNtcByIdx(int ntcIdx) throws FindException {
+		try {
+			Notice n=dao.findNtcByIdx(ntcIdx);
+			return n;
+		} catch (FindException e) {
+			throw new FindException("해당글이 없습니다.");
+		}
+	}
+	
+	public List<Notice> findNtcByWord(String word) throws FindException{
+		return dao.findNtcByWord(word);
+	}
+	
+	public void modifyNtc(Notice n) throws ModifyException{
+		dao.modifyNtc(n);
+	}
+	
+	public void removeNtc(int ntcIdx) throws RemoveException{
+		dao.removeNtc(ntcIdx);
+	}
+}
