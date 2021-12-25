a8dfcb08b816e3a933cb319cce65c1812265940a
CHIHYUNGLEE <eno125@naver.com>
2021-12-25 18:20:45 +0900

    Update SignupServlet.java

--- src/main/java/com/my/customer/control/SignupServlet.java
+++ src/main/java/com/my/customer/control/SignupServlet.java
@@ -1,6 +1,8 @@
 package com.my.customer.control;
 
 import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,6 +10,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import com.my.customer.service.CustomerService;
+import com.my.customer.vo.Customer;
+import com.my.exception.AddException;
 
 /**
  * Servlet implementation class SignupServlet
@@ -18,7 +22,35 @@ public class SignupServlet extends HttpServlet {
 	private CustomerService service = CustomerService.getInstance();
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String uName = request.getParameter("name");
+		String uNickName= request.getParameter("nickname");
+		String uEmail = request.getParameter("email");
+		String uPwd = request.getParameter("pwd");
+		Customer c = new Customer();
+		c.setUName(uName);
+		c.setUNickName(uNickName);
+		c.setUEmail(uEmail);
+		c.setUPwd(uPwd);
 		
+		String path = "jsonresult.jsp";
+		String msg = "";
+		try {
+			service.signup(c);
+//			path = "success";
+			request.setAttribute("status", 1);
+			msg = "가입성공";
+			
+		} catch (AddException e) {
+			e.printStackTrace();
+//			path = "fail";
+			request.setAttribute("status", 0);
+			msg = e.getMessage();
+			
+		}
+		request.setAttribute("msg", msg);
+		RequestDispatcher rd = request.getRequestDispatcher(path);
+		rd.forward(request, response);
 	}
-
 }
+
+
