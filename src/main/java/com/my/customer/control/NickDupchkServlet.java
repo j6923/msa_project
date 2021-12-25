322f2cccca7c480d48936d0ed845cb49749a5c8d
CHIHYUNGLEE <eno125@naver.com>
2021-12-25 18:16:21 +0900

    update front

--- src/main/java/com/my/customer/control/NickDupchkServlet.java
+++ src/main/java/com/my/customer/control/NickDupchkServlet.java
@@ -1,6 +1,8 @@
 package com.my.customer.control;
 
 import java.io.IOException;
+
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -8,6 +10,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import com.my.customer.service.CustomerService;
+import com.my.exception.FindException;
 
 /**
  * Servlet implementation class NickDupchkServlet
@@ -18,7 +21,24 @@ public class NickDupchkServlet extends HttpServlet {
 	private CustomerService service = CustomerService.getInstance();
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nicknameValue = request.getParameter("nickname");
+		String resultMsg = "";	
+		String path = "jsonresult.jsp";
 		
+		try {
+			service.nickdupchk(nicknameValue);
+			resultMsg = "이미 사용중인 닉네임입니다";
+			request.setAttribute("status", 0);
+		} catch (FindException e) {
+			resultMsg = "사용가능한 닉네임입니다";
+			request.setAttribute("status", 1);
+		}
+		
+		
+		request.setAttribute("msg", resultMsg);
+						
+		RequestDispatcher rd = request.getRequestDispatcher(path);
+		rd.forward(request, response);
 	}
 
 }
