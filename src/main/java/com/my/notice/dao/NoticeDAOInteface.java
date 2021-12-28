7ee612ca19dbe0d7676520822de433a5859cf3f0
CHIHYUNGLEE <eno125@naver.com>
2021-12-28 14:19:48 +0900

    Update NoticeDAOInteface.java

--- src/main/java/com/my/notice/dao/NoticeDAOInteface.java
+++ src/main/java/com/my/notice/dao/NoticeDAOInteface.java
@@ -53,7 +53,7 @@ public interface NoticeDAOInteface {
 	 * @param community
 	 * @throws AddException
 	 */
-	public Notice addNtc(Notice notice) throws AddException;
+	public Notice addNtc(Notice notice) throws AddException,FindException;
 	
 	
 	
@@ -62,7 +62,7 @@ public interface NoticeDAOInteface {
 	 * @param community
 	 * @throws ModifyException
 	 */
-	public Notice modifyNtc(Notice notice) throws ModifyException;
+	public Notice modifyNtc(Notice notice) throws ModifyException,FindException;
 	
 	
 	
